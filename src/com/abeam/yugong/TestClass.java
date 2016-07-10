d25bf82b9b1d5ee4b9db074122b76c60d0d178af
Gong,Yuanfeng <yugong@abeam.com>
2016-07-10 19:46:03 +0800

    thrid commit

--- src/com/abeam/yugong/TestClass.java
+++ src/com/abeam/yugong/TestClass.java
@@ -5,7 +5,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		klasjdflkajsljflkaslkfjaslkj;
 	}
 
 }
